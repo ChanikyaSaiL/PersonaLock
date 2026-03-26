1aaa04f67e73cc0f1d0ead9617a1d999517734f7
ChanikyaSaiL <lcrsai2004@gmail.com>
2026-03-27 01:31:29 +0530

    feat: add Netlify deployment config and env variable for API URL

--- client/src/components/Wizard.jsx
+++ client/src/components/Wizard.jsx
@@ -146,7 +146,7 @@ export default function Wizard() {
                 }
             });
 
-            await axios.post('http://localhost:5000/api/register', data, {
+            await axios.post(`${import.meta.env.VITE_API_URL}/api/register`, data, {
                 headers: { 'Content-Type': 'multipart/form-data' },
             });
 
